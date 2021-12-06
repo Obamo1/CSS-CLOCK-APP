b24887e875f8cedeed17dea151bf552a9fec5e35
obamo1 <joobamo@gmail.com>
2021-12-06 15:49:20 +0100

    new change

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-function setClock() { 
-    const hourHand = document.querySelector('.hours'); 
-    const minutesHand = document.querySelector('.minutes'); 
-    const secondsHand = document.querySelector('.seconds'); 
-    const now = new Date(); 
- 
-    // Offset the initial 90 degrees 
-    const hoursDegrees = getHoursDegrees(now); 
-    const minDegrees = getMinDegrees(now); 
-    const secondsDegrees = getSecondsDegrees(now); 
- 
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`; 
-    minutesHand.style.transform = `rotate(${minDegrees}deg)`; 
-    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`; 
- 
-    function getHoursDegrees(date) { 
-      return (((date.getHours() * 30) * 360) + 90 + (getMinDegrees(date) / 12)); 
-    } 
- 
-    function getMinDegrees(date) { 
-      return (((date.getMinutes() / 60) * 360) + 90 + (getSecondsDegrees(date) / 60)); 
-    } 
- 
-    function getSecondsDegrees(date) { 
-      return (((date.getSeconds() / 60) * 360) + 90); 
-    } 
-  } 
- 
-  setInterval(setClock(), 1000);
+setInterval(() => {
+	d = new Date(); //object of date()
+	hr = d.getHours();
+	min = d.getMinutes();
+	sec = d.getSeconds();
+	hr_rotation = 30 * hr + min / 2; //converting current time
+	min_rotation = 6 * min;
+	sec_rotation = 6 * sec;
+
+	hour.style.transform = `rotate(${hr_rotation}deg)`;
+	minute.style.transform = `rotate(${min_rotation}deg)`;
+	second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
